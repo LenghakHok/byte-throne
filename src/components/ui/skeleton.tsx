8862f3572f8b9ed86f2061cdda283ad4f95218ed
Lenghak Hok <lenghakhok.dev@gmail.com>
2025-04-25 12:07:38 +0700

    chore: added prettier for astro

---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/cn";
+import type React from "react";
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-accent", className)}
+      data-slot="skeleton"
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
