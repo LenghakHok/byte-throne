aca395aa3b822d6765c809dcfab85afe568fe777
Lenghak Hok <lenghakhok.dev@gmail.com>
2025-04-28 13:54:26 +0700

    feat(themes): added resolved themes

--- src/components/layouts/root.tsx
+++ src/components/layouts/root.tsx
@@ -1,9 +1,22 @@
 import { Particles } from "@/components/magic/particles";
-import { observer, useObservable } from "@legendapp/state/react";
+import { useThemes } from "@/hooks/use-themes";
+import { observable } from "@legendapp/state";
+import { observer, useObserveEffect } from "@legendapp/state/react";
 import type { PropsWithChildren } from "react";
 
+const color$ = observable("#000000");
+
 export const RootLayout = observer(({ children }: PropsWithChildren) => {
-  const color$ = useObservable("#000000");
+  const { resolvedTheme$ } = useThemes();
+
+  useObserveEffect(() => {
+    console.log(resolvedTheme$.get());
+    if (resolvedTheme$.get() === "dark") {
+      color$.set("#ffffff");
+    } else {
+      color$.set("#000000");
+    }
+  });
 
   return (
     <div className="relative flex h-dvh w-full flex-col items-center justify-center overflow-hidden">
