dc4549f47aa15e3512a4b6c42c89bdf80ea8464b
Lenghak Hok <lenghakhok.dev@gmail.com>
2025-05-02 11:58:13 +0700

    feat(dashboard): added sidebar navs

---
+++ src/domains/dashboard/composites/side-bar.tsx
@@ -0,0 +1,208 @@
+import { Logo } from "@/components/core/logo";
+import {
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  Sidebar as SidebarRoot,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import { For } from "@/utils/for";
+import {
+  BoxIcon,
+  CalendarFoldIcon,
+  CastleIcon,
+  CompassIcon,
+  CrownIcon,
+  HelpCircleIcon,
+  HomeIcon,
+  SettingsIcon,
+  SwordsIcon,
+} from "lucide-react";
+import * as React from "react";
+
+// This is sample data
+const navs = {
+  general: [
+    {
+      title: "Home",
+      url: "/dashboard",
+      icon: HomeIcon,
+      isActive: true,
+    },
+  ],
+  fun: [
+    {
+      title: "Explore",
+      url: "/explore",
+      icon: CompassIcon,
+      isActive: false,
+    },
+    {
+      title: "Competition",
+      url: "/competition",
+      icon: SwordsIcon,
+      isActive: false,
+    },
+    {
+      title: "Leaderboard",
+      url: "/leaderboard",
+      icon: CrownIcon,
+      isActive: false,
+    },
+  ],
+  management: [
+    {
+      title: "Events",
+      url: "/events",
+      icon: CalendarFoldIcon,
+      isActive: false,
+    },
+    {
+      title: "Projects",
+      url: "/projects",
+      icon: BoxIcon,
+      isActive: false,
+    },
+    {
+      title: "Teams",
+      url: "/teams",
+      icon: CastleIcon,
+      isActive: false,
+    },
+  ],
+};
+
+const misc = [
+  {
+    title: "Help",
+    url: "/help",
+    icon: HelpCircleIcon,
+    isActive: false,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: SettingsIcon,
+    isActive: false,
+  },
+];
+
+export function Sidebar({
+  ...props
+}: React.ComponentProps<typeof SidebarRoot>) {
+  // Note: I'm using state to show active item.
+  // IRL you should use the url/router.
+  const [activeItem, setActiveItem] = React.useState(navs.general[0]);
+
+  return (
+    <SidebarProvider>
+      <SidebarRoot
+        className="!w-16 overflow-hidden border-r"
+        collapsible="none"
+        {...props}
+      >
+        <SidebarHeader className="mx-auto">
+          <SidebarMenu>
+            <SidebarMenuItem className="size-fit overflow-visible">
+              <SidebarMenuButton
+                asChild={true}
+                className="size-12 overflow-visible rounded-full p-0"
+              >
+                <Logo
+                  className="p-0 [&_svg:not([class*='size-'])]:size-8"
+                  href="/dashboard"
+                />
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarSeparator className="mx-auto bg-transparent data-[orientation=horizontal]:w-8" />
+        <SidebarContent className="mx-auto">
+          <For
+            each={Object.entries(navs)}
+            render={(nav) => (
+              <React.Fragment key={`sidebar_${nav[0]}`}>
+                <SidebarGroup>
+                  <SidebarGroupContent>
+                    <SidebarMenu>
+                      <For
+                        each={nav[1]}
+                        render={(item) => (
+                          <SidebarMenuItem
+                            className="size-fit"
+                            key={`${nav[0]}_${item.title}`}
+                          >
+                            <SidebarMenuButton
+                              className="size-fit rounded-full p-3 text-muted-foreground [&>svg]:size-4.5"
+                              isActive={activeItem?.title === item.title}
+                              onClick={() => {
+                                setActiveItem(item);
+                              }}
+                              tooltip={{
+                                children: (
+                                  <span className="font-bold">
+                                    {item.title}
+                                  </span>
+                                ),
+                                hidden: false,
+                              }}
+                            >
+                              <item.icon />
+                              <span className="sr-only">{item.title}</span>
+                            </SidebarMenuButton>
+                          </SidebarMenuItem>
+                        )}
+                      />
+                    </SidebarMenu>
+                  </SidebarGroupContent>
+                </SidebarGroup>
+                <SidebarSeparator className="mx-auto data-[orientation=horizontal]:w-8" />
+              </React.Fragment>
+            )}
+          />
+        </SidebarContent>
+        <SidebarFooter className="mx-auto p-0">
+          <SidebarSeparator className="mx-auto data-[orientation=horizontal]:w-8" />
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <For
+                  each={misc}
+                  render={(item) => (
+                    <SidebarMenuItem
+                      className="size-fit"
+                      key={`misc_${item.title}`}
+                    >
+                      <SidebarMenuButton
+                        className="size-fit rounded-full p-3 text-muted-foreground [&>svg]:size-4.5"
+                        isActive={activeItem?.title === item.title}
+                        onClick={() => {
+                          setActiveItem(item);
+                        }}
+                        tooltip={{
+                          children: (
+                            <span className="font-bold">{item.title}</span>
+                          ),
+                          hidden: false,
+                        }}
+                      >
+                        <item.icon />
+                        <span className="sr-only">{item.title}</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  )}
+                />
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarFooter>
+      </SidebarRoot>
+    </SidebarProvider>
+  );
+}
